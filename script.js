89c3894b04a8e7d0711cb6b6016f72ab4b11d258
MozerBlack <joao.mozer.santos@escola.pr.gov.br>
2025-09-18 11:33:27 -0300

    cantina

---
+++ script.js
@@ -0,0 +1,12 @@
+// Passo 1 - Declarando variáveis
+console.log("--- Passo 1: Declarando variáveis ---");
+var nomeCantina = "Cantina da Escola";
+console.log("Bem-vindo à " + nomeCantina);
+
+let salgados = 20;
+console.log("Temos " + salgados + " salgados disponíveis.");
+
+const precoSalgado = 5;
+console.log("Cada salgado custa R$" + precoSalgado);
+
+// ... e o restante do seu código JavaScript
